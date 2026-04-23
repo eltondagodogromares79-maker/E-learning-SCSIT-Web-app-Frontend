617e0c122ddb548a56166f89428d3b6dc0831ab4
EltonDevs <eltondevs79@gmail.com>
2026-04-23 12:02:44 +0800

    for final revision and defense

--- web_app_frontend/app/auth_page/page.tsx
+++
@@ -1,9 +0,0 @@
-import LoginPage from '@/features/auth/components/Login';
-
-export const metadata = {
-  title: 'Sign In | SCSIT Nexus',
-};
-
-export default function AuthPage() {
-  return <LoginPage />;
-}
